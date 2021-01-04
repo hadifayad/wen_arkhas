bd5a162ab16b41446db3bf4b7cafdb99578ef41c
user <moustaphaaussie@gmail.com>
2021-01-04 18:46:11 +0200

    ...

--- app/src/main/java/com/hadi/wenarkhas/MainActivity.java
+++ app/src/main/java/com/hadi/wenarkhas/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
