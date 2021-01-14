07b7b5d7de0cb8804a508db955df9f3d8f54fd5e
user <moustaphaaussie@gmail.com>
2021-01-14 14:01:29 +0200

    add detail post

--- app/src/main/java/com/hadi/wenarkhas/activities/PostDetailActivity.java
+++ app/src/main/java/com/hadi/wenarkhas/activities/PostDetailActivity.java
@@ -1,22 +1,46 @@
 package com.hadi.wenarkhas.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.hadi.wenarkhas.R;
 import com.hadi.wenarkhas.utils.network.NetworkHelper;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
 public class PostDetailActivity extends AppCompatActivity {
 
     ImageView imageview;
     TextView textview;
+    RecyclerView comments_recyclerview;
+    EditText textInput;
+    Button btnSend;
+    String id;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +49,16 @@ public class PostDetailActivity extends AppCompatActivity {
 
         imageview = findViewById(R.id.image);
         textview = findViewById(R.id.text);
+        textInput = findViewById(R.id.textInput);
+        btnSend = findViewById(R.id.btnSend);
+        comments_recyclerview = findViewById(R.id.comments_recyclerview);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        comments_recyclerview.setLayoutManager(layoutManager);
 
         Intent intent = getIntent();
         if (intent.hasExtra("id")) {
-            String id = intent.getExtras().getString("id");
+            id = intent.getExtras().getString("id");
             String imageName = intent.getExtras().getString("imageName");
             String text = intent.getExtras().getString("text");
             String userId = intent.getExtras().getString("userId");
@@ -45,9 +75,80 @@ public class PostDetailActivity extends AppCompatActivity {
 
             textview.setText(text);
 
+            setupComments();
 
         }
 
 
+    }
+
+    public void sendComment(View view) {
+
+
+        final String text = textInput.getText().toString();
+        if (!text.equals("")) {
+            btnSend.setEnabled(false);
+            String url = NetworkHelper.getUrl(NetworkHelper.ACTION_ADD_COMMENT);
+            final StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if (response.contains("true")) {
+                                textInput.setText("");
+                                setupComments();
+                            } else {
+                                Toast.makeText(PostDetailActivity.this, "حدث خطأ", Toast.LENGTH_LONG).show();
+                            }
+                            btnSend.setEnabled(true);
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(PostDetailActivity.this, "حدث خطأ", Toast.LENGTH_LONG).show();
+                            btnSend.setEnabled(true);
+
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+
+                    Map<String, String> params = new Hashtable<String, String>();
+                    params.put("text", text);
+                    params.put("postId", id);
+                    params.put("userId", "1");
+                    return params;
+                }
+            };
+            {
+                int socketTimeout = 30000;
+                RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+                stringRequest.setRetryPolicy(policy);
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+                requestQueue.add(stringRequest);
+            }
+        }
+    }
+
+    private void setupComments() {
+//        String url = NetworkHelper.getUrl(NetworkHelper.ACTION_GET_AD_COMMENTS);
+//        Map<String, String> params = new HashMap();
+//        params.put("ad", adId);
+//        GsonRequest<Comments[]> myGsonRequest = new GsonRequest<Comments[]>(Request.Method.POST, url, Comments[].class, null, params,
+//                new Response.Listener<Comments[]>() {
+//                    @Override
+//                    public void onResponse(Comments[] response) {
+//                        comments_recyclerview.setAdapter(new CommentsRecyclerViewAdapter(Arrays.asList(response)));
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//
+//                        NetworkHelper.handleError(error);
+//                    }
+//                });
+//
+//        VolleySingleton.getInstance(AdDetailActivity.this).addToRequestQueue(myGsonRequest);
     }
 }
---
+++ app/src/main/java/com/hadi/wenarkhas/models/Comment.java
@@ -0,0 +1,57 @@
+package com.hadi.wenarkhas.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Comment {
+
+    @SerializedName("id")
+    String id;
+    @SerializedName("c_text")
+    String c_text;
+    @SerializedName("r_post ")
+    String r_post;
+    @SerializedName("r_user ")
+    String r_user;
+    @SerializedName("creation_date")
+    String creation_date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getC_text() {
+        return c_text;
+    }
+
+    public void setC_text(String c_text) {
+        this.c_text = c_text;
+    }
+
+    public String getR_post() {
+        return r_post;
+    }
+
+    public void setR_post(String r_post) {
+        this.r_post = r_post;
+    }
+
+    public String getR_user() {
+        return r_user;
+    }
+
+    public void setR_user(String r_user) {
+        this.r_user = r_user;
+    }
+
+    public String getCreation_date() {
+        return creation_date;
+    }
+
+    public void setCreation_date(String creation_date) {
+        this.creation_date = creation_date;
+    }
+}
